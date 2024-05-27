c6e3dea78ee3d821c07d97c0b4915837878e0737
Álvaro Gascón <alvarogascon@hotmail.com>
2024-05-27 14:34:38 +0200

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Álvaro Gascón on 27/5/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
